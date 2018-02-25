f21b2427d2fe3aec06b1a7da21288d04c3399676
Avishka007 <telanb@aol.co.uk>
2018-02-25 17:18:51 +0530

    name app

---
+++ 1st.py
@@ -0,0 +1,9 @@
+x = input ("enter your first name: ")
+y = input ("enter your secoend name: ")
+z = input ("enter your family name:  ")
+
+a = (x+ " " + y + " " + z + " " )
+print (a)
+print ("welcome" +" "+ x + "!")
+
+
